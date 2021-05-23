7744e92053e74017ec8fe5c97b4c79cb01dc3ff6
Rasmus Bengtsson <rasmus@beryllium-tech.eu>
2021-05-23 10:33:55 +0200

    Finished delete image mvp

--- src/dockerService/Handlers/DeleteImageHandler.cs
+++ src/dockerService/Handlers/DeleteImageHandler.cs
@@ -1,24 +1,53 @@
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using BreakableLime.DockerBackgroundService.models.external;
 using BreakableLime.DockerBackgroundService.models.external.ActionSpecifications;
+using BreakableLime.GlobalModels;
+using BreakableLime.GlobalModels.Wrappers;
 using Docker.DotNet;
 using Docker.DotNet.Models;
+using Microsoft.Extensions.Logging;
 
 namespace BreakableLime.DockerBackgroundService.Handlers
 {
     public class DeleteImageHandler : DockerActionHandlerBase
     {
+        private readonly ILogger<DeleteImageHandler> _logger;
         private readonly DeleteImageSpecification _spec;
 
-        public DeleteImageHandler(IDockerClient dockerClient, DockerWorkItem actionSpecification) : base(dockerClient, actionSpecification)
+        public DeleteImageHandler(IDockerClient dockerClient, 
+            DockerWorkItem actionSpecification, 
+            ILogger<DeleteImageHandler> logger) : base(dockerClient, actionSpecification)
         {
+            _logger = logger;
             _spec = actionSpecification.SpecificationsMarker as DeleteImageSpecification;
         }
 
-        public override Task Execute()
+        public override async Task Execute()
         {
-            
-            throw new System.NotImplementedException();
+            try
+            {
+                var result = await DockerClient.Images.DeleteImageAsync(_spec.ImageId, new ImageDeleteParameters(),
+                    ActionSpecification.CancellationToken);
+
+                if (result.Any())
+                {
+                    _logger.LogInformation("Successfully deleted image {Id}", _spec.ImageId);
+                    _spec.ReturnStore.ReturnedValue = Result<Empty>.FromResult<Empty>(new Empty());
+                }
+                else
+                {
+                    _logger.LogError("Unable to delete image {Id}", _spec.ImageId);
+                    _spec.ReturnStore.ReturnedValue = Result<Empty>.FromError<Empty>("Unable to delete image");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical(ex, "Unable to delete image {Id}, exception was thrown", _spec.ImageId);
+                _spec.ReturnStore.ReturnedValue = Result<Empty>.FromError<Empty>("Unable to delete image, exception was thrown");
+                return;
+            }
         }
     }
 }
--- src/dockerService/Handlers/Factories/DeleteImageHandlerFactory.cs
+++ src/dockerService/Handlers/Factories/DeleteImageHandlerFactory.cs
@@ -1,15 +1,22 @@
 using BreakableLime.DockerBackgroundService.models.external;
 using Docker.DotNet;
+using Microsoft.Extensions.Logging;
 
 namespace BreakableLime.DockerBackgroundService.Handlers.Factories
 {
     public class DeleteImageHandlerFactory : DockerActionHandlerFactoryBase
     {
-        public DeleteImageHandlerFactory(DockerClient dockerClient) : base(dockerClient)
+        private readonly ILoggerFactory _loggerFactory;
+
+        public DeleteImageHandlerFactory(DockerClient dockerClient, ILoggerFactory loggerFactory) : base(dockerClient)
         {
+            _loggerFactory = loggerFactory;
         }
 
-        public override IDockerActionHandler Create(DockerWorkItem actionSpecification) =>
-            new DeleteImageHandler(DockerClient, actionSpecification);
+        public override IDockerActionHandler Create(DockerWorkItem actionSpecification)
+        {
+            var logger = _loggerFactory.CreateLogger<DeleteImageHandler>();
+            return new DeleteImageHandler(DockerClient, actionSpecification, logger);
+        }
     }
 }
